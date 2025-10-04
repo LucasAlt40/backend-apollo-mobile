5a3fa5425a91a8d0209f57f285852edb7f61cf2b
Eduardo Pires Carvalho <epc44129@gmail.com>
2025-10-04 18:22:22 -0300

    fix: troca nome Playlist Response

--- src/main/java/br/com/apollomusic/app/domain/payload/response/PlaylistResponse.java
+++ src/main/java/br/com/apollomusic/app/domain/payload/response/PlaylistResponse.java
@@ -5,5 +5,5 @@ import br.com.apollomusic.app.domain.payload.Image;
 import java.util.Collection;
 import java.util.Map;
 // GENERO AQUI
-public record PlaylistResponse(String id, String name, String description, Collection<Image> images, Map<String, Integer> initialGenres, Map<String, Integer> blockedGenres, Map<String, Integer> genres, boolean hasIncrementedGenre ) {
+public record PlaylistResponse(String id, String name, String description, Collection<Image> images, Map<String, Integer> initialArtists, Map<String, Integer> blockedArtists, Map<String, Integer> artists, boolean hasIncrementedArtist ) {
 }
